8824fd21300bc69ca3a38d9b6ed300d3e0ef0977
SaberDoTcodeR <45599139+SaberDoTcodeR@users.noreply.github.com>
2020-01-27 02:34:22 +0330

    issue #3 enhancement

--- client/webrtc.js
+++ client/webrtc.js
@@ -4,13 +4,13 @@ var remoteVideo;
 var peerConnection;
 var uuid;
 var serverConnection;
-var dest_UUID
+var dest_UUID;
 
 var channel;
 
 var peerConnectionConfig = {
     'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19302'}//should be updated
+        {'urls': 'stun:194.225.43.38:3478'}//should be updated
 
     ]
 };
@@ -67,7 +67,7 @@ function handleReceiveChannelStatusChange(event) {
     // when the channel's status changes.
 }
 
-async function pageReady() {
+function pageReady() {
 
 
     localVideo = document.getElementById('localVideo');
@@ -82,7 +82,7 @@ async function pageReady() {
     };
 
     if (navigator.mediaDevices.getUserMedia) {
-        await navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
+        navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
 
     } else {
         alert('Your browser does not support getUserMedia API');
@@ -101,15 +101,18 @@ function start(isCaller) {
 
     peerConnection.ontrack = gotRemoteStream;
 
-
     openDataChannel();
     peerConnection.ondatachannel = receiveChannelCallback;
 
 
     if (isCaller) {
         dest_UUID = prompt("dest UUID", "UUID");
-        peerConnection.createOffer().then(createdDescription).catch(errorHandler);
-        peerConnection.addStream(localStream);
+        serverConnection.send(JSON.stringify({
+            "msg_id": 8,
+            'uuid': uuid,
+            'dest_uuid': dest_UUID
+        }));
+
 
     }
 
@@ -118,53 +121,67 @@ function start(isCaller) {
 
 function gotMessageFromServer(message) {
     if (!peerConnection) start(false);
-
     var signal = JSON.parse(message.data);
-    console.log(signal)
-    if ("error" in signal) {
-        alert(signal.error);
-        return;
-    }
-    if ("cancel" in signal) {
-        if (signal.cancel) {
-            alert("heyy");
-            cancel();
+
+
+    console.log(signal);
+    switch (signal.msg_id) {
+        case 1:
+            uuid = signal.uuid;
+            console.log(uuid);
             return;
-        }
-    }
-    if ("given_uuid" in signal) {
-        uuid = signal.given_uuid;
-        console.log(uuid)
-        return;
-    }
+        case 2:
+            if (peerConnection.remoteDescription) {
+                peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+            }
+            return;
 
-    // Ignore messages from ourself
-    if (signal.uuid == uuid) return;
-
-    if (signal.sdp) {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function () {
-            // Only create answers in response to offers
-            if (signal.sdp.type == 'offer') {
-                if (confirm('You have an offer from ' + signal.uuid + ' ,do you want to start p2p connetion?')) {
+        case 3:
+            alert(signal.error);
+            return;
+        case 4:
+            peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function () {
+                if (signal.sdp.type === 'offer') {
                     dest_UUID = signal.uuid;
                     peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
-                    serverConnection.send(JSON.stringify({'cancel': false, 'uuid': uuid}));
-
-                } else {
-                    serverConnection.send(JSON.stringify({'uuid': uuid, 'dest_uuid': signal.uuid, 'refuse': true}));
-
+                    serverConnection.send(JSON.stringify({"msg_id": 7, 'uuid': uuid}));
                 }
+
+            }).catch(errorHandler);
+            return;
+
+        case 5:
+            alert("heyy");
+            cancel(false);
+            return;
+        case 8:
+            if (!confirm('You have an offer from ' + signal.dest_uuid + ' ,do you want to start p2p connection?')) {
+                serverConnection.send(JSON.stringify({"msg_id": 6, 'uuid': uuid, 'dest_uuid': signal.uuid}));
+            } else {
+                start(false);
+                serverConnection.send(JSON.stringify({"msg_id": 9, 'uuid': uuid, 'dest_uuid': signal.uuid}));
+                peerConnection.addStream(localStream);
             }
-        }).catch(errorHandler);
-    } else if (signal.ice) {
-        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
-        peerConnection.addStream(localStream);
+            return;
+        case 9:
+            peerConnection.createOffer().then(createdDescription).catch(errorHandler);
+            peerConnection.addStream(localStream);
+            return;
+        default:
+
+
     }
+
 }
 
 function gotIceCandidate(event) {
     if (event.candidate != null) {
-        serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid, 'dest_uuid': dest_UUID}));
+        serverConnection.send(JSON.stringify({
+            "msg_id": 2,
+            'ice': event.candidate,
+            'uuid': uuid,
+            'dest_uuid': dest_UUID
+        }));
     }
 }
 
@@ -172,6 +189,7 @@ function createdDescription(description) {
 
     peerConnection.setLocalDescription(description).then(function () {
         serverConnection.send(JSON.stringify({
+            "msg_id": 4,
             'sdp': peerConnection.localDescription,
             'uuid': uuid,
             'dest_uuid': dest_UUID
@@ -197,7 +215,7 @@ function openDataChannel() {
             reliable: false
         }
     );
-
+    console.log("asd");
     channel.onmessage = function (event) {
         console.log(event.data);
     };
@@ -220,10 +238,11 @@ function openDataChannel() {
 
 }
 
-function cancel() {
-    serverConnection.send(JSON.stringify({'cancel': true, 'uuid': uuid, 'dest_uuid': dest_UUID}));
-    peerConnection.close();
-    dest_UUID = null
+function cancel(is_starter) {
+    if (is_starter)
+        serverConnection.send(JSON.stringify({"msg_id": 5, 'uuid': uuid, 'dest_uuid': dest_UUID}));
+    dest_UUID = null;
+    remoteVideo.srcObject = null;
     messageInputBox.disabled = true;
     sendButton.disabled = true;
     channel.close();
